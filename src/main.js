372e764870fdbdacdf5fe678716225b7e04ff5e0
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2023-11-14 03:19:03 -0500

    Giving a default image for the posters that have an error in the loading proccess

--- src/main.js
+++ src/main.js
@@ -38,6 +38,13 @@ function createMovies(movies, container, lazyLoad = false) {
             'https://image.tmdb.org/t/p/w300' + movie.poster_path,
         );
 
+        movieImg.addEventListener('error', () => {
+            movieImg.setAttribute(
+                'src',
+                'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+            );
+        });
+
         if (lazyLoad) {
             lazyLoader.observe(movieImg);
         }
@@ -97,7 +104,7 @@ async function getMoviesByCategory(id) {
 
     const movies = data.results;
     
-    createMovies(movies, genericSection);
+    createMovies(movies, genericSection, true);
 
 }
 
